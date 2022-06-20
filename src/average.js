c15d3a9b93c166efe21f8edcc526b75654ad28f0
Leckar <gusleckar@gmail.com>
2022-06-20 15:38:58 -0300

    Completed up to requisite 1

--- src/average.js
+++ src/average.js
@@ -12,17 +12,16 @@
 */
 
 const average = (arr) => {
-  if (arr.every(e => typeof e === 'number') && arr.length !== 0) {
+  if (arr.every((e) => (typeof e === 'number')) && arr.length !== 0) {
     let sum = 0;
     arr.map((e) => {
       sum += e;
+      return sum;
     });
     const avg = Math.round(sum / arr.length);
     return avg;
-
   }
   return undefined;
 };
 
 module.exports = average;
-
